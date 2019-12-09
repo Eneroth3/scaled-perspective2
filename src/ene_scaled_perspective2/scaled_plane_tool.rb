111cf2a3a74c3f582e24fda36b47e2f888f229cf
Eneroth3 <eneroth3@gmail.com>
2019-12-09 11:34:44 +0100

    Screen space to target plane space

--- src/ene_scaled_perspective2/scaled_plane_tool.rb
+++ src/ene_scaled_perspective2/scaled_plane_tool.rb
@@ -10,6 +10,9 @@ module Eneroth
       # Color to draw plane with.
       COLOR = Sketchup::Color.new(127, 127, 127, 0.6)
 
+      # Size of grid in logical pixels.
+      GRID_SIZE = 40
+
       # @api
       # @see https://ruby.sketchup.com/Sketchup/Tool.html
       def activate
@@ -122,18 +125,66 @@ module Eneroth
       def draw_plane
         # TODO: Draw fancy checkered pattern.
         view = Sketchup.active_model.active_view
-        points = plane_corners
+        points = plane_corners.values_at(0, 1, 3, 2)
         view.drawing_color = COLOR
         view.draw(GL_QUADS, points)
+
+        # TEST CODE
+        transformation = screen_to_plane
+        points = [
+          Geom::Point3d.new(0, 0, 0),
+          Geom::Point3d.new(100, 200, 0),
+          Geom::Point3d.new(200, 200, 0)
+        ]
+
+        # To 3d space
+        points.map! { |pt| pt.transform(transformation) }
+
+        # To screen space anew
+        ### points.map! { |pt| view.screen_coords(pt) }
+
+        p points
+        p points.map { |pt| pt.on_plane?(plane) }
+
+        view.drawing_color = "red"
+        view.line_width = 10
+        ### view.draw2d(GL_LINE_STRIP, points)
+        view.line_stipple = ""
+        view.draw(GL_LINE_STRIP, points)
+        # Some weird Z-fighting with quad, but works otherwise.
+      end
+
+      # Transformation for converting logical screen coordinates to scale plane
+      # 3d coordinates.
+      def screen_to_plane
+        corners = plane_corners
+        origin = corners[0]
+        xaxis = corners[1] - corners[0]
+        yaxis = corners[2] - corners[0]
+        zaxis = (xaxis * yaxis).normalize
+
+        # REVIEW: Is this logical pixels or physical pixels? I want logical!
+        xaxis.length /= Sketchup.active_model.active_view.vpwidth
+        yaxis.length /= Sketchup.active_model.active_view.vpheight
+
+        new_transformation(origin, xaxis, yaxis, zaxis)
+      end
+
+      def new_transformation(origin, xaxis, yaxis, zaxis)
+        Geom::Transformation.new([
+          xaxis.x,  xaxis.y,  xaxis.z,  0,
+          yaxis.x,  yaxis.y,  yaxis.z,  0,
+          zaxis.x,  zaxis.y,  zaxis.z,  0,
+          origin.x, origin.y, origin.z, 1
+        ])
       end
 
       def plane_corners
         view = Sketchup.active_model.active_view
+
         points = Array.new(4) do |i|
           Geom.intersect_line_plane(view.pickray(view.corner(i)), plane)
         end
-
-        points.values_at(0, 1, 3, 2)
       end
 
       # Only valid if at leas one InputPoint is valid.
